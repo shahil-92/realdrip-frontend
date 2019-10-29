32788249b786cbf1ccfcfffa96edee8350f11ce0
harry <shahil.virtual@gmail.com>
2019-10-30 01:55:45 +0530

    connected inuse page ui component desigining.

--- src/pages/connectedInuse/index.js
+++ src/pages/connectedInuse/index.js
@@ -62,50 +62,63 @@ export class ConnectedInuse extends React.Component {
                
                 <div className="inner_dash">
                   <div className="left-mid-dash">
-                 
                    <div className="wrap_ontxt">
-                      <span className="b2_txt">B2</span>
-                      <span className="wrap_on"><span className="on_txt">On</span> <span className="round_green_circle"></span></span>
+                      <div className="wrap_b2_txt"><span className="b2_txt">B2</span></div>
+                      <div className="wrap_on_txt"><div className="on_txt">On</div> <div className="round_green_circle"></div></div>
                    </div>
-                
+                   <div className="wrap_label_main_wrap">
+                      <div className="label_wrapper"><div className="Label_b2_name">Label</div><div className="small_txt">B2</div></div>
+                      <div className="label_wrapper pdng_right_label"><div className="Label_b2_name">ID</div><div className="small_txt">02113322</div></div>
+                   </div>
+
+
+                  <div className="edit_label_wrap">Edit Label</div>
                  
+                
                   <div className="wrap_left_section">
-                    <div className="dash_ward_device_wrap oprtn_wrap">
-                    
-                    <div className="infusn_wrap device_infusn oprtn_dtl_wrd">
-                          <div className="wrpd_icon_num">
-                            <span className="urgnt_txt">
-                                <h3 className="prcnt_opt_detail">89%</h3>
-                                <h5 className="prcnt_op_txt">Dispense 462cl</h5>
-                                <div className="wrapper_progress_bar"><div className="inner_progress_bar"></div></div>
-                            </span>
-                            <span className="drip_img_wrap"><img src={require('../../assets/Images/drip.png')} /></span>
-                          </div>
-                     </div>
+                    <div className="dash_ward_device_wrap oprtn_wrap connect_left_top_margin">
+                    <h4 class="sumry_head">Status</h4>
+                    <div className="status_wrapper">
+                        <div className="status_icon_txt_wrap"><div className="con_text">Connected</div> <div className="status_img_wrap"><img src={require('../../assets/Images/drip.png')} /></div></div>
+                        <div className="status_icon_txt_wrap"><div className="con_text">In use</div> <div className="status_img_wrap"><img src={require('../../assets/Images/drip.png')} /></div></div>
+                    </div>
+                    <h4 class="sumry_head">Summary</h4>
+                   
                      <div className="infusn_wrap device_infusn  oprtn_dtl_wrd">
                           <div className="wrpd_icon_num">
                             <span className="urgnt_txt">
-                                <h3 className="prcnt_opt_detail">36%</h3>
-                                <h5 className="prcnt_op_txt">Dispense 462cl</h5>
+                                <h3 className="prcnt_opt_detail">28</h3>
+                                <h5 className="prcnt_op_txt">All Operations</h5>
                             </span>
                             <span className="drip_img_wrap"><img src={require('../../assets/Images/drip.png')} /></span>
                           </div>
                      </div>
-                     <div className="infusn_wrap device_infusn  oprtn_dtl_wrd">
-                     <div className="wrpd_icon_num">
-                            <span className="urgnt_txt">
-                                <h3 className="prcnt_opt_detail">89%</h3>
-                                <h5 className="prcnt_op_txt">Dispense 462cl</h5>
-                                <div className="wrapper_progress_bar"><div className="inner_progress_bar"></div></div>
-                            </span>
-                            <span className="drip_img_wrap"><img src={require('../../assets/Images/drip.png')} /></span>
+                     <div className="infusn_device_wrap">
+                        <div className="infusn_wrap">
+                          <div className="wrpd_icon_num">
+                            <span className="urgnt_txt">45</span>
+                          
                           </div>
-                     </div>
+                          <h5>Active Infusion</h5>
+                        </div>
+                        <div className="infusn_wrap">
+                          <div className="wrpd_icon_num">
+                            <span className="urgnt_txt">43</span>
+                            {/* <span className="drip_img_wrap"></span> */}
+                          </div>
+                          <h5>Active Infusion</h5>
+                        </div>
+                      </div>
 
                     </div>
                     </div>
                   </div>
                   <div className="right-mid-dash">
+                  <h4 class="infusion_heading">Infusion Operation</h4>
+                    <div className="history_insuse_tabs_wrap">
+                        <div className="con_text active">In use</div>
+                        <div className="con_text">History</div>
+                    </div>
                     <div className="right-wrap-heading devics_lst_clmn">  
                       <h3>Doctor Instruction</h3>
                       <div class="dash_active_wrap whitebg">
