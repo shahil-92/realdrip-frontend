419f2269342666cc74993f36748afe054bed5241
harry <shahil.virtual@gmail.com>
2019-12-04 23:34:20 +0530

    dashboard api.

--- src/pages/wardDashboard/index.js
+++ src/pages/wardDashboard/index.js
@@ -20,9 +20,32 @@ export class WardDashBoard extends React.Component {
   handleRedirectClick = () =>{
     this.props.history.push('/operation-detail')
  }
+ activeNurseList = (data) => {
+  //  const {data} = this.props.WardNurse.wardNurse
+   data && data.map((data)=>{
+    return(
+      <ul className="list_heading_ul ul_contnt active_dsh_list bottom">    
+        <li>
+          <div className="user_img_nme_wrap">
+            <img class="user_img" src={require('../../assets/Images/user.png')}></img>
+            <span>{data.name}</span>
+          </div>    
+        </li>
+        <li>
+          <div className="time_in_second bld_txt time_btm">Frank Joseph</div>
+        </li>
+        <li>
+          <div className="time_only time_btm">12:23pm</div>
+        </li>
+     </ul>
+    )
+   })
+ }
 
   render() {
-    console.log('=-=-=-=-=',this.props)
+    console.log('===',this.props)
+    const {wardNurse} = this.props.WardNurse
+    const {wardInfusion} = this.props.WardInfusion
     return (
         <div className="main_wrapper ">
            <div className="inner_dshbrd_wrap">
@@ -130,6 +153,7 @@ export class WardDashBoard extends React.Component {
                     </div>
                     </div>
                   </div>
+
                   <div className="right-mid-dash">
                     <div className="right-wrap-heading"> 
                       <div className="infusion_wrap">
@@ -144,9 +168,13 @@ export class WardDashBoard extends React.Component {
                           <li>Nurse</li>
                           <li>Patient Case</li>
                         </ul>
-                        <ul className="list_heading_ul ul_contnt active_dsh_list" onClick={()=>this.handleRedirectClick()}>    
+                        
+                        {/* startVolume */}
+
+                        {wardInfusion && wardInfusion.data.map((data)=>(
+                          <ul className="list_heading_ul ul_contnt active_dsh_list" onClick={()=>this.handleRedirectClick()}>    
                           <li>
-                            <div className="bld_prcnt">89%</div>
+                            <div className="bld_prcnt">{data.startVolume}</div>
                             <div className="wrapper_progress_bar"><div className="inner_progress_bar"></div></div>
                             <div className="bld_txt">Blood</div>
                           </li>
@@ -167,106 +195,36 @@ export class WardDashBoard extends React.Component {
                             <div className="infusn_disease_name">Malaria</div>
                           </li>
                         </ul>
-
-                        <ul className="list_heading_ul ul_contnt">    
-                          <li>
-                            <div className="bld_prcnt">89%</div>
-                            <div className="wrapper_progress_bar"><div className="inner_progress_bar"></div></div>
-                            <div className="bld_txt">Blood</div>
-                          </li>
-                          <li>
-                            <div className="time_in_second bld_txt">00:13:00</div>
-                            <div className="time_only">12:23pm</div>
-                          </li>
-                          <li>
-                            <div className="speed_hr"> <span> 28 </span> <img class="speed_up" src={require('../../assets/Images/dummy_logo.jpg')}></img></div>
-                            <div className="speed_unit bld_txt">ml/hr</div>
-                          </li>
-                          <li>
-                            <div className="infusn_usr_img"><img src={require('../../assets/Images/user.png')}></img></div>
-                            <div className="infusn_usr_name">Titlayo Olaide</div>
-                          </li>
-                          <li>
-                            <div className="infusn_usr_fname">Frank</div>
-                            <div className="infusn_disease_name">Malaria</div>
-                          </li>
-                        </ul>
-
-
-                        <ul className="list_heading_ul ul_contnt">    
-                          <li>
-                            <div className="bld_prcnt">89%</div>
-                            <div className="wrapper_progress_bar"><div className="inner_progress_bar"></div></div>
-                            <div className="bld_txt">Blood</div>
-                          </li>
-                          <li>
-                            <div className="time_in_second bld_txt">00:13:00</div>
-                            <div className="time_only">12:23pm</div>
-                          </li>
-                          <li>
-                            <div className="speed_hr"> <span> 28 </span> <img class="speed_up" src={require('../../assets/Images/dummy_logo.jpg')}></img></div>
-                            <div className="speed_unit bld_txt">ml/hr</div>
-                          </li>
-                          <li>
-                            <div className="infusn_usr_img"><img src={require('../../assets/Images/user.png')}></img></div>
-                            <div className="infusn_usr_name">Titlayo Olaide</div>
-                          </li>
-                          <li>
-                            <div className="infusn_usr_fname">Frank</div>
-                            <div className="infusn_disease_name">Malaria</div>
-                          </li>
-                        </ul>
+                      ))}
                       </div>
                     
                     
                       <div className="infusion_wrap bottom">
-                        <h4 class="sumry_head2">Active Infusion</h4>
+                        <h4 class="sumry_head2">Active Nurses</h4>
                         <span class="see_all_txt">see all</span>
                       </div>
                       <div className="active_dsh_list">
-                      <ul className="list_heading_ul ul_contnt active_dsh_list bottom">    
-                          <li>
-                            <div className="user_img_nme_wrap">
-                              <img class="user_img" src={require('../../assets/Images/user.png')}></img>
-                              <span>Frank Joseph</span>
-                            </div>    
-                          </li>
-                          <li>
-                            <div className="time_in_second bld_txt time_btm">Frank Joseph</div>
-                          </li>
-                          <li>
-                            <div className="time_only time_btm">12:23pm</div>
-                          </li>
-                      </ul>
+
+
+     
+                      {wardNurse && wardNurse.data.map((data)=>(
+                        <ul className="list_heading_ul ul_contnt active_dsh_list bottom">    
+                        <li>
+                          <div className="user_img_nme_wrap">
+                            <img class="user_img" src={require('../../assets/Images/user.png')}></img>
+                            <span>{data.name}</span>
+                          </div>    
+                        </li>
+                        <li>
+                          <div className="time_in_second bld_txt time_btm">- - - - -</div>
+                        </li>
+                        <li>
+                          <div className="time_only time_btm">12:23pm</div>
+                        </li>
+                      </ul> 
+                      ))}
                       
-                      <ul className="list_heading_ul ul_contnt active_dsh_list bottom">    
-                          <li>
-                            <div className="user_img_nme_wrap">
-                              <img class="user_img" src={require('../../assets/Images/user.png')}></img>
-                              <span>Frank Joseph</span>
-                            </div>    
-                          </li>
-                          <li>
-                            <div className="time_in_second bld_txt time_btm">- - - - -</div>
-                          </li>
-                          <li>
-                            <div className="time_only time_btm">12:23pm</div>
-                          </li>
-                      </ul>
-                      <ul className="list_heading_ul ul_contnt active_dsh_list bottom">    
-                          <li>
-                            <div className="user_img_nme_wrap">
-                              <img class="user_img" src={require('../../assets/Images/user.png')}></img>
-                              <span>Frank Joseph</span>
-                            </div>    
-                          </li>
-                          <li>
-                            <div className="time_in_second bld_txt time_btm">Frank Joseph</div>
-                          </li>
-                          <li>
-                            <div className="time_only time_btm">12:23pm</div>
-                          </li>
-                      </ul>
+                       {wardNurse.data && this.activeNurseList(wardNurse.data)}
                       </div>
                     </div>
                   </div>
